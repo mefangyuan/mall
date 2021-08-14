9e2b5ed88517674ed0e014e46d9e1c7d07a7c1c4
wangfangyuan <mefangyuan@gmail.com>
2021-08-14 11:04:37 +0800

    初始提交同时增加商品品牌crud

---
+++ mall-mbg/src/main/java/com/mawoz/mall/mapper/OmsCartItemMapper.java
@@ -0,0 +1,30 @@
+package com.mawoz.mall.mapper;
+
+import com.mawoz.mall.model.OmsCartItem;
+import com.mawoz.mall.model.OmsCartItemExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OmsCartItemMapper {
+    long countByExample(OmsCartItemExample example);
+
+    int deleteByExample(OmsCartItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(OmsCartItem record);
+
+    int insertSelective(OmsCartItem record);
+
+    List<OmsCartItem> selectByExample(OmsCartItemExample example);
+
+    OmsCartItem selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") OmsCartItem record, @Param("example") OmsCartItemExample example);
+
+    int updateByExample(@Param("record") OmsCartItem record, @Param("example") OmsCartItemExample example);
+
+    int updateByPrimaryKeySelective(OmsCartItem record);
+
+    int updateByPrimaryKey(OmsCartItem record);
+}
